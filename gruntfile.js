348603ce5fcf84636947e2b0090c146630471ac4
Somi <somy.taheri@gmail.com>
2015-03-07 22:58:41 +1100

    services basic html structure created

--- gruntfile.js
+++ gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function (grunt) {
             },
 
             clean: {
-                html: ['public/*']
+                html: ['public/*.html']
             },
 
             copy: {
